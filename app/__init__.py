42b7ad55bd1b557e647635d46af49e6ed4082a15
sarai <aldrinhao@gmail.com>
2020-06-17 13:47:13 +0800

    conf

--- app/__init__.py
+++ app/__init__.py
@@ -5,46 +5,34 @@ from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_environments import Environments
 
 
-service_account = 'sarai-522@decoded-academy-219803.iam.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(
-    service_account, 'privatekey.json')
+
+
+
+
 
 app = Flask(__name__, instance_relative_config=True)
 
-
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+env = Environments(app)
+env.from_yaml(os.path.join(os.getcwd(), 'conf/main.yml'))
+
+ee_api_config = app.config['EARTH_ENGINE_API']
+private_key_file = os.path.join(os.getcwd(), ee_api_config['PRIVATE_KEY'])
+service_account = os.path.join(os.getcwd(), ee_api_config['ACCOUNT'])
+
+credentials = ee.ServiceAccountCredentials(service_account, private_key_file)
 
 
 def create_app(test_config=None):
 
-    # create and configure the app
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://deploy:sarai111@localhost/sarai_maps_db'
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
     @app.route('/')
     def hello():
-        return 'Hello, World!'
+        return 'API is working'
+
 
     from . import ndvi, evi, crops, chirps
     app.register_blueprint(ndvi.bp)
